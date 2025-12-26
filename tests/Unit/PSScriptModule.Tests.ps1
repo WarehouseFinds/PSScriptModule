96bafcbcf92becbfab78c7a412972bb5ce3bfe24
Marko Stanojevic <m.stanojevic@outlook.com>
2025-12-26 23:41:56 +0100

    Fix typo in test paths

--- tests/Unit/PSScriptModule.Tests.ps1
+++ tests/Unit/PSScriptModule.Tests.ps1
@@ -1,12 +1,12 @@
 # Generate Pester Unit tests for the module
 Describe "PSScriptModule Unit Tests" {
     It "Have at least 1 Public Function" {
-        $publicFunctions = Get-ChildItem -Path "$moduleSourcePath\Public\*.ps1" -Exclude "*.Tests.*"
+        $publicFunctions = Get-ChildItem -Path "$script:moduleSourcePath\Public\*.ps1" -Exclude "*.Tests.*"
         $publicFunctions.Count | Should -BeGreaterThan 0
     }
 
     It "Have at least 1 Private Function" {
-        $privateFunctions = Get-ChildItem -Path "$moduleSourcePath\Private\*.ps1" -Exclude "*.Tests.*"
+        $privateFunctions = Get-ChildItem -Path "$script:moduleSourcePath\Private\*.ps1" -Exclude "*.Tests.*"
         $privateFunctions.Count | Should -BeGreaterThan 0
     }
 }
